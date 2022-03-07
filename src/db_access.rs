ccd55b3d4624ef1bfbab10e096e97a7443f3a9f0
SNiclas <dev@s-niclas.eu>
2022-03-07 15:06:04 +0100

    feat: Basic database-config

---
+++ src/db_access.rs
@@ -0,0 +1,106 @@
+use std::borrow::Cow;
+use mongodb::{Client, Database};
+use mongodb::bson::doc;
+use mongodb::options::ClientOptions;
+use thiserror::Error;
+use serde::{Serialize, Deserialize};
+use serde::de::DeserializeOwned;
+use crate::db_access::DBError::{NoDocumentFoundError, QueryError, ServerConnectionError};
+use std::str::FromStr;
+use std::sync::Mutex;
+use mongodb::bson::oid::ObjectId;
+
+// Database-Identifier
+const DB_NAME: &str = "test";
+// Collection-Identifier
+pub const NOTES: &str = "notes";
+pub const CREDENTIALS: &str = "creds";
+pub const USER: &str = "user";
+
+// Schemata
+// Sub-Structures
+#[derive(Debug, Serialize, Deserialize)]
+pub enum AllowanceLevel {
+    Read,
+    ReadWrite,
+    Owner
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Allowance {
+    pub note_id: String,
+    pub level: AllowanceLevel
+}
+
+// Database-Objects
+pub trait DatabaseObject: DeserializeOwned + Unpin + Send + Sync {}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Credential {
+    pub _id: String,
+    pub passwd: String //TODO DO NOT STORE PASSWORDS IN PLAIN TEXT
+}
+impl DatabaseObject for Credential {}
+impl Credential {
+    pub fn verify(&self, passwd: &Cow<'static, str>) -> bool {
+        self.passwd.eq(passwd.to_string().as_str()) //TODO DO NOT COMPARE PASSWORDS IN PLAIN TEXT
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub _id: String,
+    pub allowances: Vec<Allowance>,
+    pub connections: Vec<String>
+}
+impl DatabaseObject for User {}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Note {
+    pub title: String,
+    pub content: String,
+    pub owner_id: String,
+    pub tags: Vec<String>
+}
+impl DatabaseObject for Note {}
+
+// Error-Types
+#[derive(Error, Debug)]
+pub enum DBError {
+    #[error("wrong credentials")]
+    WrongCredentialsError,
+    #[error("could not connect to database-server")]
+    ServerConnectionError,
+    #[error("query returned an error")]
+    QueryError,
+    #[error("no document found")]
+    NoDocumentFoundError
+}
+
+pub async fn connect_to_database(uri: (String, String), cred: (String, String)) -> Result<Database, DBError> {
+    // Configure the connection
+    let mut client_options = ClientOptions::parse(format!("mongodb://{}:{}@{}:{}", cred.0, cred.1, uri.0, uri.1)).await.unwrap();
+    client_options.app_name = Some("writeUp".to_string());
+    // Attempt to connect
+    let client = Client::with_options(client_options).unwrap();
+    let db = client.database(DB_NAME);
+    // Test the connection
+    db.run_command(doc! {"ping": 1}, None).await.map(|_|db).map_err(|_|ServerConnectionError)
+}
+
+pub async fn get_dbo_by_id<T: DatabaseObject>(collection: &str, id: String, db: &Mutex<Database>) -> Result<T,DBError> {
+    // Prepare the query
+    let coll = db.lock().unwrap().collection::<T>(collection);
+    let filter;
+    if collection.eq(NOTES) {
+        filter = doc! {"_id": ObjectId::from_str(id.as_str()).unwrap()};
+    } else {
+        filter = doc! {"_id": id};
+    }
+    // Match the result
+    match coll.find_one(filter, None).await {
+        Ok(Some(doc)) => Ok(doc),
+        Ok(None) => Err(NoDocumentFoundError),
+        Err(_) => Err(QueryError)
+    }
+}
--- src/web/mod.rs
+++ src/web/mod.rs
@@ -5,13 +5,17 @@ mod user;
 mod share;
 mod error;
 
+use std::sync::Mutex;
 use serde::{Serialize, Deserialize};
-use actix_web::{get, HttpResponse, Responder, web};
+use actix_web::{get, HttpResponse, Responder, web::{ServiceConfig, Data}};
 use actix_web_httpauth::extractors::{basic::BasicAuth, bearer::BearerAuth};
 use chrono::Utc;
 use jsonwebtoken::{Algorithm, decode, DecodingKey, encode, EncodingKey, Header, Validation};
+use mongodb::{bson::doc, Database};
+use crate::db_access::{AllowanceLevel, Credential, CREDENTIALS, DBError, get_dbo_by_id, Note, NOTES, User, USER};
 use crate::web::error::AuthError;
 
+// JWT-Assets
 const JWT_SECRET: &[u8] = env!("JWT_SECRET").as_bytes(); //TODO secret is static
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -20,12 +24,14 @@ struct Claims {  // Credits to: https://blog.logrocket.com/jwt-authentication-in
     exp: usize,
 }
 
+// Response-Objects
 #[derive(Serialize)]
-struct TokenResponse {
-    token: String
+struct NoteResponse {
+    note: Note,
+    allowance: AllowanceLevel
 }
 
-pub fn handler_config(cfg: &mut web::ServiceConfig) {
+pub fn handler_config(cfg: &mut ServiceConfig) {
     // Add all special handler
     cfg.service(authenticate)
         .service(list_notes);
@@ -46,19 +52,63 @@ pub fn handler_config(cfg: &mut web::ServiceConfig) {
 }
 
 #[get("/auth")]
-async fn authenticate(auth: BasicAuth) -> impl Responder {
-    //TODO Verify credentials
-    match create_jwt(auth.user_id()) {
-        Ok(jwt) => HttpResponse::Ok().json(TokenResponse {token: jwt}),
-        Err(e) => e.gen_response()
+async fn authenticate(auth: BasicAuth, db: Data<Mutex<Database>>) -> impl Responder {
+    #[derive(Serialize)]
+    struct TokenResponse {
+        token: String
+    }
+    match get_dbo_by_id::<Credential>(CREDENTIALS, auth.user_id().to_string(), db.get_ref()).await {
+        Ok(cred) => {
+            if cred.verify(auth.password().unwrap()) {
+                create_jwt(auth.user_id()).map(|jwt| HttpResponse::Ok()
+                    .json(TokenResponse {token: jwt})).unwrap_or_else(|e| e.gen_response())
+            } else { HttpResponse::Forbidden().finish() } //wrong password
+        }
+        Err(DBError::NoDocumentFoundError) => HttpResponse::Forbidden().finish(), //No user with that username has been found
+        Err(_) => HttpResponse::InternalServerError().finish() //Unknown
     }
 }
 
 #[get("/notes")]
-async fn list_notes(auth: BearerAuth) -> impl Responder { //TODO implement
+async fn list_notes(auth: BearerAuth, db: Data<Mutex<Database>>) -> impl Responder {
+    // Define Response-Object
+    #[derive(Serialize)]
+    struct ReducedNoteResponse {
+        note_id: String,
+        title: String,
+        tags: Vec<String>,
+        allowance: AllowanceLevel
+    }
+    // Verify Credentials
     let user = get_user_from_jwt(auth.token());
     match user {
-        Ok(user_id) => HttpResponse::Ok().json(format!("Request for listing all owned and shared notes of '{}' received", user_id)),
+        Ok(user_id) => {
+            // Get list of allowed notes
+            match get_dbo_by_id::<User>(USER, user_id, db.get_ref()).await {
+                Ok(user) => {
+                    let mut response_vector = Vec::new();
+                    for allowance in user.allowances {
+                        // Read all allowed notes and create response-objects
+                        match get_dbo_by_id::<Note>(NOTES, allowance.note_id.clone(), db.get_ref()).await {
+                            Ok(note) => response_vector.push(ReducedNoteResponse {
+                                note_id: allowance.note_id,
+                                title: note.title,
+                                tags: note.tags,
+                                allowance: allowance.level
+                            }),
+                            Err(DBError::NoDocumentFoundError) => return HttpResponse::InternalServerError()
+                                .json(format!("Reference to deleted note(ID:{}) still exists", allowance.note_id)), //user has allowance for a nonexisting note)
+                            Err(_) => {} //unknown
+                        }
+                    }
+                    // Return the compiled list of notes
+                    HttpResponse::Ok().json(response_vector)
+                }
+                Err(DBError::NoDocumentFoundError) => HttpResponse::InternalServerError()
+                    .json("User-Object is missing"), //there are credentials for a nonexisting user
+                Err(_) => HttpResponse::InternalServerError().finish() //unknown
+            }
+        }
         Err(e) => e.gen_response()
     }
 }
