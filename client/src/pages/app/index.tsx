04864aff467ab88d3da59aee134e809a47a1a848
xDarkyne <darkyne.gaming@outlook.de>
2022-04-03 00:09:04 +0200

    feat: added auto closing for some symbols (e.g. brackets)

--- client/src/pages/app/index.tsx
+++ client/src/pages/app/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { SyntheticEvent, useContext, useEffect, useState } from 'react';
+import { SyntheticEvent, useContext, useEffect, useRef, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import remarkEmoji from 'remark-emoji';
@@ -8,11 +8,21 @@ import { CodeBlock } from 'components';
 import { dApi } from 'lib';
 import { UserContext } from 'context';
 
+const closing: Record<string, string> = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>',
+  '"': '"',
+};
+
 const Home: NextPage = () => {
   const [notes, setNotes] = useState<INoteShallow[]>([]);
   const [curNote, setCurNote] = useState<INote | undefined>(undefined);
   const [newTitle, setNewTitle] = useState<string>('');
   const { currentUser, loading } = useContext(UserContext);
+
+  const editor = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
     if (typeof window === 'undefined') return;
@@ -95,46 +105,70 @@ const Home: NextPage = () => {
           </ul>
         </div>
 
-        <div className="grid">
-          <textarea
-            name="input"
-            spellCheck="false"
-            id="input"
-            value={curNote?.note.content}
-            onChange={({ target }) => {
-              if (!curNote) return;
-              setCurNote({
-                ...curNote,
-                note: {
-                  ...curNote.note,
-                  content: target.value,
-                },
-              });
-            }}
-          />
-          <div className="preview">
-            <ReactMarkdown
-              remarkPlugins={[remarkGfm, remarkEmoji]}
-              components={{
-                code({ node, inline, className, children, ...props }) {
-                  const match = /language-(\w+)/.exec(className || '');
-                  return !inline && match ? (
-                    <CodeBlock
-                      value={String(children).replace(/\n$/, '')}
-                      language={match[1]}
-                    />
-                  ) : (
-                    <code className={className} {...props}>
-                      {children}
-                    </code>
-                  );
-                },
+        {curNote && (
+          <div className="grid">
+            <textarea
+              name="input"
+              spellCheck="false"
+              id="input"
+              value={curNote?.note.content}
+              onKeyUp={(e) => {
+                const key = e.key;
+
+                if (!closing[key]) return;
+
+                const pos = e.currentTarget.selectionStart;
+                const value = e.currentTarget.value;
+                e.currentTarget.value = [
+                  value.slice(0, pos),
+                  closing[key],
+                  value.slice(pos),
+                ].join('');
+                e.currentTarget.selectionEnd = pos;
+
+                setCurNote({
+                  ...curNote,
+                  note: {
+                    ...curNote.note,
+                    content: e.currentTarget.value,
+                  },
+                });
               }}
-            >
-              {curNote?.note.content ?? ''}
-            </ReactMarkdown>
+              onChange={(e) => {
+                if (!curNote) return;
+                setCurNote({
+                  ...curNote,
+                  note: {
+                    ...curNote.note,
+                    content: e.target.value,
+                  },
+                });
+              }}
+            />
+            <div className="preview">
+              <ReactMarkdown
+                remarkPlugins={[remarkGfm, remarkEmoji]}
+                components={{
+                  code({ node, inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || '');
+                    return !inline && match ? (
+                      <CodeBlock
+                        value={String(children).replace(/\n$/, '')}
+                        language={match[1]}
+                      />
+                    ) : (
+                      <code className={className} {...props}>
+                        {children}
+                      </code>
+                    );
+                  },
+                }}
+              >
+                {curNote?.note.content ?? ''}
+              </ReactMarkdown>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </>
   );
