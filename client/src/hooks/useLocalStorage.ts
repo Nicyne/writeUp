7dc35d827c2fae6d400660094c0bb6a1b3547fc7
xDarkyne <darkyne.gaming@outlook.de>
2022-11-04 01:50:12 +0100

    fix(typing): added typing to return values and memoized setter

--- client/src/hooks/useLocalStorage.ts
+++ client/src/hooks/useLocalStorage.ts
@@ -1,6 +1,9 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
-export function useLocalStorage<T>(key: string, initValue: T) {
+export function useLocalStorage<T>(
+  key: string,
+  initValue: T
+): [T, (value: T) => void] {
   const [storedValue, setStoredValue] = useState(() => {
     if (typeof window === 'undefined') {
       return initValue;
@@ -15,18 +18,21 @@ export function useLocalStorage<T>(key: string, initValue: T) {
     }
   });
 
-  const setValue = (value: T) => {
-    try {
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      setStoredValue(valueToStore);
-      if (typeof window !== 'undefined') {
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+  const setValue = useCallback(
+    (value: T) => {
+      try {
+        const valueToStore =
+          value instanceof Function ? value(storedValue) : value;
+        setStoredValue(valueToStore);
+        if (typeof window !== 'undefined') {
+          window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    },
+    [key, storedValue]
+  );
 
   return [storedValue, setValue];
 }
